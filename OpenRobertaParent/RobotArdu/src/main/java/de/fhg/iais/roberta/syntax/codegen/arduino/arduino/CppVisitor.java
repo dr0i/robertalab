067eeb3aeb3c9e7c43c744a682fbb3e346a927d1
Evgeniya Ovchinnikova <elhed@yandex.ru>
2018-04-12 12:10:04 +0200

    #848 genrated code for most of the sensors

--- OpenRobertaParent/RobotArdu/src/main/java/de/fhg/iais/roberta/syntax/codegen/arduino/arduino/CppVisitor.java
+++ OpenRobertaParent/RobotArdu/src/main/java/de/fhg/iais/roberta/syntax/codegen/arduino/arduino/CppVisitor.java
@@ -38,6 +38,7 @@ import de.fhg.iais.roberta.syntax.sensor.generic.ColorSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.DropSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.EncoderSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.HumiditySensor;
+import de.fhg.iais.roberta.syntax.sensor.generic.InfraredSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.LightSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.MoistureSensor;
 import de.fhg.iais.roberta.syntax.sensor.generic.MotionSensor;
@@ -189,8 +190,8 @@ public class CppVisitor extends ArduinoVisitor implements ArduinoAstVisitor<Void
 
     @Override
     public Void visitLightSensor(LightSensor<Void> lightSensor) {
-        this.sb.append("analogRead(_eingang_" + lightSensor.getPort().getPortNumber() + ")");
-        //this.sb.append("analogRead(eingang_" + lightSensor.getBlockName() + ")");
+        this.sb.append("analogRead(_output_" + lightSensor.getPort().getPortNumber() + ")");
+        //this.sb.append("analogRead(_output_" + lightSensor.getBlockName() + ")");
         return null;
     }
 
@@ -218,7 +219,7 @@ public class CppVisitor extends ArduinoVisitor implements ArduinoAstVisitor<Void
 
     @Override
     public Void visitTemperatureSensor(TemperatureSensor<Void> temperatureSensor) {
-        this.sb.append("temperature");
+        this.sb.append("analogRead(_TMP36_" + temperatureSensor.getPort().getPortNumber() + ")");
         return null;
     }
 
@@ -229,20 +230,21 @@ public class CppVisitor extends ArduinoVisitor implements ArduinoAstVisitor<Void
     }
 
     @Override
-    public Void visitVoltageSensor(VoltageSensor<Void> voltageSensor) {
-        this.sb.append("voltage");
+    public Void visitVoltageSensor(VoltageSensor<Void> potentiometer) {
+        this.sb.append("analogRead(_output_" + potentiometer.getPort().getPortNumber() + ")*_sensingToVolts");
         return null;
     }
 
     @Override
     public Void visitHumiditySensor(HumiditySensor<Void> humiditySensor) {
-        this.sb.append("humidity");
+        //TODO: temperatutre and humidity modes
+        this.sb.append("_dht_" + humiditySensor.getPort().getPortNumber() + ".readHumidity()");
         return null;
     }
 
     @Override
     public Void visitDropSensor(DropSensor<Void> dropSensor) {
-        this.sb.append("drop");
+        this.sb.append("analogRead(_S_" + dropSensor.getPort().getPortNumber() + ")");
         return null;
     }
 
@@ -254,13 +256,20 @@ public class CppVisitor extends ArduinoVisitor implements ArduinoAstVisitor<Void
 
     @Override
     public Void visitRfidSensor(RfidSensor<Void> rfidSensor) {
-        this.sb.append("rfid");
+        //TODO: presence and serial number modes. Add a lib function that would read the code
+        this.sb.append("rob.rfidSerial(_mfrc522_" + rfidSensor.getPort().getPortNumber() + ")");
+        return null;
+    }
+
+    @Override
+    public Void visitInfraredSensor(InfraredSensor<Void> infraredSensor) {
+        this.sb.append("irrecv" + infraredSensor.getPort().getPortNumber() + ".decode(&_results_" + infraredSensor.getPort().getPortNumber() + ")");
         return null;
     }
 
     @Override
     public Void visitMotionSensor(MotionSensor<Void> motionSensor) {
-        this.sb.append("motion");
+        this.sb.append("digitalRead(_output_" + motionSensor.getPort().getPortNumber() + ") == HIGH");
         return null;
     }
 
@@ -295,6 +304,14 @@ public class CppVisitor extends ArduinoVisitor implements ArduinoAstVisitor<Void
             return;
         }
         this.sb.append("#include <math.h> \n");
+        //TODO: only include if needed:
+        //if IR is used
+        this.sb.append("#include <Irremote.h> \n");
+        //if humidity is used
+        this.sb.append("#include <DHT.h> \n");
+        //if RFID is used
+        this.sb.append("#include <SPI.h> \n");
+        this.sb.append("#include <MFRC522.h> \n");
     }
 
     @Override
@@ -307,15 +324,45 @@ public class CppVisitor extends ArduinoVisitor implements ArduinoAstVisitor<Void
     private void generateConfigurationSetup() {
         for ( UsedConfigurationBlock usedConfigurationBlock : this.usedConfigurationBlocks ) {
             switch ( (ConfigurationBlockType) usedConfigurationBlock.getType() ) {
+                case HUMIDITY:
+                    this.sb.append("_dht_" + usedConfigurationBlock.getBlockName() + "();");
+                    nlIndent();
+                    break;
                 case ULTRASONIC:
                     this.sb.append("pinMode(_trigger_" + usedConfigurationBlock.getBlockName() + ", OUTPUT);");
                     nlIndent();
                     this.sb.append("pinMode(_echo_" + usedConfigurationBlock.getBlockName() + ", INPUT);");
                     nlIndent();
                     break;
+                case MOTION:
+                    this.sb.append("pinMode(_output_" + usedConfigurationBlock.getBlockName() + ", INPUT);");
+                    nlIndent();
+                    break;
                 case MOISTURE:
                     break;
+                case INFRARED:
+                    this.sb.append("pinMode(13, OUTPUT);");
+                    nlIndent();
+                    this.sb.append("irrecv" + usedConfigurationBlock.getBlockName() + ".enableIRIn();");
+                    nlIndent();
+                    break;
                 case LIGHT:
+                    break;
+                case POTENTIOMETER:
+                    break;
+                case TEMPERATURE:
+                    break;
+                case ENCODER:
+                    break;
+                case DROP:
+                    break;
+                case PULSE:
+                    break;
+                case RFID:
+                    this.sb.append("SPI.begin();");
+                    nlIndent();
+                    this.sb.append("_mfrc522_" + usedConfigurationBlock.getBlockName() + ".PCD_Init();");
+                    nlIndent();
                     break;
                 default:
                     throw new DbcException("Sensor is not supported: " + usedConfigurationBlock.getType());
@@ -325,23 +372,69 @@ public class CppVisitor extends ArduinoVisitor implements ArduinoAstVisitor<Void
 
     private void generateConfigurationVariables() {
         for ( UsedConfigurationBlock usedConfigurationBlock : this.usedConfigurationBlocks ) {
+            String blockName = usedConfigurationBlock.getBlockName();
             switch ( (ConfigurationBlockType) usedConfigurationBlock.getType() ) {
-                case ULTRASONIC:
-                    this.sb.append("int _trigger_" + usedConfigurationBlock.getBlockName() + " = ").append(usedConfigurationBlock.getPins().get(0)).append(";");
+                case HUMIDITY:
+                    this.sb.append("#define DHTPIN" + blockName + " ").append(usedConfigurationBlock.getPins().get(0));
                     nlIndent();
-                    this.sb.append("int _echo_" + usedConfigurationBlock.getBlockName() + " = ").append(usedConfigurationBlock.getPins().get(1)).append(";");
+                    this.sb.append("#define DHTTYPE DHT11");
+                    nlIndent();
+                    this.sb.append("DHT _dht_" + blockName + "(DHTPIN" + blockName + ", DHTTYPE);");
+                    nlIndent();
+                    break;
+                case ULTRASONIC:
+                    this.sb.append("int _trigger_" + blockName + " = ").append(usedConfigurationBlock.getPins().get(0)).append(";");
+                    nlIndent();
+                    this.sb.append("int _echo_" + blockName + " = ").append(usedConfigurationBlock.getPins().get(1)).append(";");
                     nlIndent();
                     this.sb.append(
                         "double _signalToDistance = 0.03432/2; //Nun berechnet man die Entfernung in Zentimetern. Man teilt zunächst die Zeit durch zwei (Weil man ja nur eine Strecke berechnen möchte und nicht die Strecke hin- und zurück). Den Wert multipliziert man mit der Schallgeschwindigkeit in der Einheit Zentimeter/Mikrosekunde und erhält dann den Wert in Zentimetern.");
                     nlIndent();
                     break;
                 case MOISTURE:
-                    this.sb.append("int _moisturePin_" + usedConfigurationBlock.getBlockName() + " = ").append(usedConfigurationBlock.getPins().get(0)).append(
-                        ";");
+                    this.sb.append("int _moisturePin_" + blockName + " = ").append(usedConfigurationBlock.getPins().get(0)).append(";");
+                    nlIndent();
+                    break;
+                case INFRARED:
+                    this.sb.append("int _RECV_PIN_" + blockName + " = ").append(usedConfigurationBlock.getPins().get(0)).append(";");
+                    nlIndent();
+                    this.sb.append("IRrecv(_RECV_PIN_" + blockName + ");");
+                    nlIndent();
+                    this.sb.append("decode_results _results_" + blockName + ";");
                     nlIndent();
                     break;
                 case LIGHT:
-                    this.sb.append("int _eingang_" + usedConfigurationBlock.getBlockName() + " = ").append(usedConfigurationBlock.getPins().get(0)).append(";");
+                    this.sb.append("int _output_" + blockName + " = ").append(usedConfigurationBlock.getPins().get(0)).append(";");
+                    nlIndent();
+                    break;
+                case MOTION:
+                    this.sb.append("int _output_" + blockName + " = ").append(usedConfigurationBlock.getPins().get(0)).append(";");
+                    nlIndent();
+                    break;
+                case POTENTIOMETER:
+                    this.sb.append("int _output_" + blockName + " = ").append(usedConfigurationBlock.getPins().get(0)).append(";");
+                    nlIndent();
+                    this.sb.append("double _sensingToVolts = 5/1023;");
+                    nlIndent();
+                    break;
+                case TEMPERATURE:
+                    this.sb.append("int _TMP36_" + blockName + " = ").append(usedConfigurationBlock.getPins().get(0)).append(";");
+                    nlIndent();
+                    break;
+                case ENCODER:
+                    break;
+                case DROP:
+                    this.sb.append("int _S_" + blockName + " = ").append(usedConfigurationBlock.getPins().get(0)).append(";");
+                    nlIndent();
+                    break;
+                case PULSE:
+                    break;
+                case RFID:
+                    this.sb.append("#define SS_PIN_" + blockName + " " + usedConfigurationBlock.getPins().get(0));
+                    nlIndent();
+                    this.sb.append("#define RST_PIN_" + blockName + " " + usedConfigurationBlock.getPins().get(1));
+                    nlIndent();
+                    this.sb.append("MFRC522 _mfrc522_" + blockName + "(SS_PIN_" + blockName + ", RST_PIN_" + blockName + ");");
                     nlIndent();
                     break;
                 default:
